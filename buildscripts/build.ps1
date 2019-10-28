d392896c66d6b3caff41701f5c53db56355ec05f
Adam Bertram <adam@adams-mbp.lan>
2019-10-28 12:26:44 -0500

    CD prep

--- buildscripts/build.ps1
+++ buildscripts/build.ps1
@@ -6,16 +6,21 @@ try {
     $manifestContent = Get-Content -Path $manifestFilePath -Raw
 
     ## Update the module version based on the build version and limit exported functions
+    $publicFunctions = Get-ChildItem -Path "$env:APPVEYOR_BUILD_FOLDER\Public" | Select-Object -ExpandProperty BaseName
+    $functionsToExport = ($publicFunctions | foreach { "'$_'" }) -join ','
     $replacements = @{
-        "ModuleVersion = '.*'" = "ModuleVersion = '$env:APPVEYOR_BUILD_VERSION'"
+        "ModuleVersion\s+=\s+'\*'"     = "ModuleVersion = '$env:APPVEYOR_BUILD_VERSION'"
+        "FunctionsToExport\s+=\s+'\*'" = "FunctionsToExport = $functionsToExport"
     }		
 
     $replacements.GetEnumerator() | foreach {
+        Write-Host "Replacing [$($_.Key)] with [$($_.Value)]..."
         $manifestContent = $manifestContent -replace $_.Key, $_.Value
     }
 
     $manifestContent | Set-Content -Path $manifestFilePath
 
 } catch {
+    Write-Error -Message $_.Exception.Message
     $host.SetShouldExit($LastExitCode)
 }
