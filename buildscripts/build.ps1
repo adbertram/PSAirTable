c6ae6976f32c9faf13220f8510ab6dee88e17081
Adam Bertram <adam@adams-mbp.lan>
2019-10-28 12:24:04 -0500

    CD prep

--- buildscripts/build.ps1
+++ buildscripts/build.ps1
@@ -7,7 +7,7 @@ try {
 
     ## Update the module version based on the build version and limit exported functions
     $replacements = @{
-        "ModuleVersion = '\*'" = "ModuleVersion = '$env:APPVEYOR_BUILD_VERSION'"
+        "ModuleVersion = '.*'" = "ModuleVersion = '$env:APPVEYOR_BUILD_VERSION'"
     }		
 
     $replacements.GetEnumerator() | foreach {
