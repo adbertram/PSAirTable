4ad5d82853b8ccc7624ed4adfce615f7b25be911
Adam Bertram <adam@adams-mbp.lan>
2019-10-28 12:09:26 -0500

    CD prep work

--- buildscripts/install.ps1
+++ buildscripts/install.ps1
@@ -1,16 +1,27 @@
-$provParams = @{
-    Name           = 'NuGet'
-    MinimumVersion = '2.8.5.208'
-    Force          = $true
-}
+$ErrorActionPreference = 'Stop'
 
-$null = Install-PackageProvider @provParams
-$null = Import-PackageProvider @provParams
+try {
 
-$requiredModules = @('PowerShellGet')
-foreach ($m in $requiredModules) {
-    Write-Host "Installing module [$($m)]..."
-    Install-Module -Name $m -Force -Confirm:$false
-    Remove-Module -Name $m -Force -ErrorAction Ignore
-    Import-Module -Name $m
+    $provParams = @{
+        Name           = 'NuGet'
+        MinimumVersion = '2.8.5.208'
+        Force          = $true
+    }
+
+    $null = Install-PackageProvider @provParams
+    $null = Import-PackageProvider @provParams
+
+    $requiredModules = @('PSPostMan')
+    foreach ($m in $requiredModules) {
+        Write-Host "Installing [$($m)] module..."
+        Install-Module -Name $m -Force -Confirm:$false
+        Write-Host "Removing [$($m)] module from current session..."
+        Remove-Module -Name $m -Force -ErrorAction Ignore
+        Write-Host "Importing [$($m)] module into current session..."
+        Import-Module -Name $m
+    }
+
+} catch {
+    Write-Error -Message $_.Exception.Message
+    $host.SetShouldExit($LastExitCode)
 }
--- buildscripts/publish.ps1
+++ buildscripts/publish.ps1
@@ -1,33 +1,33 @@
 $ErrorActionPreference = 'Stop'
 
 try {
-	## Don't upload the build scripts and appveyor.yml to PowerShell Gallery
-	$tempmoduleFolderPath = "$env:Temp\PSPostMan"
-	$null = mkdir $tempmoduleFolderPath
+    ## Don't upload the build scripts and appveyor.yml to PowerShell Gallery
+    $tempmoduleFolderPath = "$env:Temp\PSAirTable"
+    $null = mkdir $tempmoduleFolderPath
 
-	## Remove all of the files/folders to exclude out of the main folder
-	$excludeFromPublish = @(
-		'PSPostMan\\buildscripts'
-		'PSPostMan\\appveyor\.yml'
-		'PSPostMan\\\.git'
-		'PSPostMan\\\.nuspec'
-		'PSPostMan\\README\.md'
-		'PSPostMan\\TestResults\.xml'
-	)
-	$exclude = $excludeFromPublish -join '|'
-	Get-ChildItem -Path $env:APPVEYOR_BUILD_FOLDER -Recurse | where { $_.FullName -match $exclude } | Remove-Item -Force -Recurse
+    ## Remove all of the files/folders to exclude out of the main folder
+    $excludeFromPublish = @(
+        'PSAirTable\\buildscripts'
+        'PSAirTable\\appveyor\.yml'
+        'PSAirTable\\\.git'
+        'PSAirTable\\\.nuspec'
+        'PSAirTable\\README\.md'
+        'PSAirTable\\TestResults\.xml'
+    )
+    $exclude = $excludeFromPublish -join '|'
+    Get-ChildItem -Path $env:APPVEYOR_BUILD_FOLDER -Recurse | where { $_.FullName -match $exclude } | Remove-Item -Force -Recurse
 
-	## Publish module to PowerShell Gallery
-	$publishParams = @{
-		Path = $env:APPVEYOR_BUILD_FOLDER
-		NuGetApiKey = $env:nuget_apikey
-		Repository = 'PSGallery'
-		Force = $true
-		Confirm = $false
-	}
-	Publish-Module @publishParams
+    ## Publish module to PowerShell Gallery
+    $publishParams = @{
+        Path        = $env:APPVEYOR_BUILD_FOLDER
+        NuGetApiKey = $env:nuget_apikey
+        Repository  = 'PSGallery'
+        Force       = $true
+        Confirm     = $false
+    }
+    Publish-Module @publishParams
 
 } catch {
-	Write-Error -Message $_.Exception.Message
-	$host.SetShouldExit($LastExitCode)
+    Write-Error -Message $_.Exception.Message
+    $host.SetShouldExit($LastExitCode)
 }
