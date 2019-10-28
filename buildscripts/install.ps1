9c1cc1ba7f7909a17b2f21b9de089435a62571c1
Adam Bertram <adam@adams-mbp.lan>
2019-10-28 12:17:28 -0500

    CD prep

--- buildscripts/install.ps1
+++ buildscripts/install.ps1
@@ -2,12 +2,19 @@ $ErrorActionPreference = 'Stop'
 
 try {
 
-    Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
+    $provParams = @{
+        Name           = 'NuGet'
+        MinimumVersion = '2.8.5.208'
+        Force          = $true
+    }
+
+    $null = Install-PackageProvider @provParams
+    $null = Import-PackageProvider @provParams
 
     $requiredModules = @('PSPostMan')
     foreach ($m in $requiredModules) {
         Write-Host "Installing [$($m)] module..."
-        Install-Module -Name $m -Force
+        Install-Module -Name $m -Force -Confirm:$false
         Write-Host "Removing [$($m)] module from current session..."
         Remove-Module -Name $m -Force -ErrorAction Ignore
         Write-Host "Importing [$($m)] module into current session..."
