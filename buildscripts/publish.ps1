a4a9732fc7b981544ff0d53b101e69efac6a6a02
Adam Bertram <adbertram@gmail.com>
2024-03-01 08:12:22 -0600

    build updates

--- buildscripts/publish.ps1
+++ buildscripts/publish.ps1
@@ -2,9 +2,6 @@ $ErrorActionPreference = 'Stop'
 
 try {
     ## Don't upload the build scripts and appveyor.yml to PowerShell Gallery
-    $tempmoduleFolderPath = "$env:Temp\PSAirTable"
-    $null = mkdir $tempmoduleFolderPath
-
     ## Remove all of the files/folders to exclude out of the main folder
     $excludeFromPublish = @(
         'PSAirTable\\buildscripts'
@@ -14,7 +11,7 @@ try {
         'PSAirTable\\README\.md'
     )
     $exclude = $excludeFromPublish -join '|'
-    Get-ChildItem -Path $env:APPVEYOR_BUILD_FOLDER -Recurse | where { $_.FullName -match $exclude } | Remove-Item -Force -Recurse
+    Get-ChildItem -Path $env:APPVEYOR_BUILD_FOLDER -Recurse | Where-Object { $_.FullName -match $exclude } | Remove-Item -Force -Recurse
 
     ## Publish module to PowerShell Gallery
     $publishParams = @{
